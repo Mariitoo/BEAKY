402560fbbaa91329a45968c72076d881acecfb7a
Mario Oria <mariito230501@gmail.com>
2022-03-05 12:40:45 -0600

    menu principal

--- BEAKY/Presentacion/MenuPrincipal.cs
+++ BEAKY/Presentacion/MenuPrincipal.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BEAKY
+namespace BEAKY.Presentacion
 {
-    public partial class Form1 : Form
+    public partial class MenuPrincipal : Form
     {
-        public Form1()
+        public MenuPrincipal()
         {
             InitializeComponent();
         }
