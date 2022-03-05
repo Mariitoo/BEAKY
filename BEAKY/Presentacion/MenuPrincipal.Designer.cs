98ffac13253dc7d44faa20662f8e420c18cdf0c8
Mario Oria <mariito230501@gmail.com>
2022-03-05 13:06:55 -0600

    cambios de panel a la izquierda

--- BEAKY/Presentacion/MenuPrincipal.Designer.cs
+++ BEAKY/Presentacion/MenuPrincipal.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(77, 12);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(129, 346);
+            this.panel1.Size = new System.Drawing.Size(196, 575);
             this.panel1.TabIndex = 0;
             // 
             // panel2
